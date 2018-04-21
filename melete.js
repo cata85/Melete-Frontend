9a951fee0e43e5b7512a003d3ba1078264f15e96
Carter <cartern@ksu.edu>
2018-04-21 14:30:40 -0500

    more

--- melete.js
+++ melete.js
@@ -1,21 +1,19 @@
 var subDivCount = 1;
-
-var editorDiv = document.getElementById('source');
-var Range = require('ace/range').Range;
-var editor = ace.edit('source');
-
-// Initialize ace
-editor.getSession().setMode('ace/mode/javascript');
-editor.setFontSize(12);
-editor.setOption("maxLines", 100);
+var top = 50;
+var left = 600;
 
 
 function displayFunction(code) {
-    // Temporary
+    // Temporary, may not need subDivCount or divID!
     var div = document.createElement("div");
     div.id = "func" + subDivCount.toString();
+    div.style.border = "1px solid lightgray";
+    div.style.marginTop = top + "px";
+    div.style.marginLeft = left + "px";
+    div.style.width = "30%";
     document.body.insertBefore(div, null);
     subDivCount++;
+    top+= 1000;
     code = "function reverse(str) {\n" +
            "    if (str === \"\") {\n" +
            "        return\"\";\n" +
@@ -31,3 +29,25 @@ function displayFunction(code) {
     var lineNumber = tempEditor.session.getLength();
     tempEditor.gotoLine(lineNumber);
 }
+
+
+var editorDiv = document.getElementById('source');
+var Range = require('ace/range').Range;
+var editor = ace.edit('source');
+
+// Initialize ace
+editor.getSession().setMode('ace/mode/javascript');
+editor.setFontSize(12);
+editor.setOption("maxLines", 100);
+editor.session.setValue(
+                        "function reverse(str) {\n" +
+                        "    if (str === \"\") {\n" +
+                        "        return\"\";\n" +
+                        "    }else{\n" +
+                        "        return reverse(str.substr(i)) + str[0];\n" +
+                        "    }\n" +
+                        "}\n\n" +
+                        "var str = \"hello\";\n" +
+                        "console.log(reverse(str));"
+                        );
+editor.gotoLine(editor.session.getLength());
